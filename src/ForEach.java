be56601710058ee4421c8101e4a6fd0a55f5217d
Jose3708k <josenahumacunalopez8@gmail.com>
2024-07-01 20:31:49 -0600

    First commit

---
+++ src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+  String[] animals = {"dog","cat","horse","shark","snake","dolphin","whale",
+  "donkey","monkey"};
+  for (String animal:animals){
+      System.out.println(animal);
+  }
+    }
+}
